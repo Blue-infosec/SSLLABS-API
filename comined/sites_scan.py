421ed7138e9972f53d319c9f60ec358b017f3b2c
Pradyumna Joshi <joshi.pradyumna@gmail.com>
2018-06-29 18:09:47 +0530

    Add files via upload

--- comined/sites_scan.py
+++ comined/sites_scan.py
@@ -6,6 +6,11 @@ import argparse
 import yaml
 from utils.ssllabs_api import SSLLabsAPI
 from utils.parse_report import Parse_SSLLabs
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
+from utils.manage_elasticsearch import Manage_Elasticsearch
+from datetime import datetime, timedelta
+
 import ast
 from pprint import pprint
 
@@ -23,6 +28,38 @@ def read_config(yaml_file):
         configuration_data = yaml.load(f)
     return configuration_data
 
+def update_elasticsearch(elastic_config,domain):
+    bulk_records = 500
+    try:
+        #elasticsearch index name
+        es_index = elastic_config['index']
+        es_doctype = elastic_config['doc_type']
+        es_host = elastic_config['host']
+        es_port = elastic_config['port']
+
+        # create elasticsearch instance
+        el_instance = Manage_Elasticsearch(es_host,
+                                   es_port,
+                                   es_index,
+                                   es_doctype,bulk_records)
+        es_health = el_instance.check_health()
+        if not es_health:
+            logger.info("Failed to connect to Elasticsearch server." 
+                        " Kindly re-check Elasticsearch settings"
+                        " and then try again")
+            sys.exit(1) 
+
+        # check if elasticsearch index exists. If not, create a new elastic index.
+        index_present = el_instance.index_exists(es_index)
+        if not index_present: 
+            response = el_instance.create_index(es_index)
+
+        # update ssllabs-api results to elasticsearch
+        el_instance.update_dict(data)
+        logger.info("SSL Labs API report for domain %s is written to elasticsearch successfully." %domain)
+    except Exception,e:
+        logger.error("Error while updating ssl report in elasticsearch database - %s" %e.message,exc_info=True)
+
 
 def cmd_arguments():
 
@@ -35,6 +72,7 @@ def cmd_arguments():
         return args
     except Exception as exc:
         logger.error("Error while getting command line arguments - %s" %exc.message,exc_info=True)
+
 
 if __name__ == "__main__":
     try:
@@ -60,9 +98,11 @@ if __name__ == "__main__":
                 #    json_result = ast.literal_eval(read_json)
 
                 parse_report = Parse_SSLLabs(domain_results)
-                pprint (parse_report.get_results())
+                # get parsed report
+                parsed_report = parse_report.get_results()
+                # update report in elasticsearch
+                update_elasticsearch(config['elastic'],domain)  
             sys.exit(1)
-
 
     except Exception,e:
         logger.error("Error while running the script - %s" % e.message,exc_info=True)
